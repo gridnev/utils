a17cd3f3de87739fee4f8c1e0cabd35bc7b6f805
dgridnev <denis.gridnev@gmail.com>
2021-03-02 07:33:05 +0300

    some fixes

--- utils.go
+++ utils.go
@@ -9,11 +9,12 @@ func InSlice(a []string, x string) bool {
 	return false
 }
 
-func ContainsInt(a []int, x int) bool {
+func InSliceInt(a []int, x int) bool {
 	for _, n := range a {
 		if x == n {
 			return true
 		}
 	}
+
 	return false
 }
